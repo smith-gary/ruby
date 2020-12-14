24078439cc08786a21e837e574e12875e18ff742
Gary S <69396671+tacgee@users.noreply.github.com>
2020-12-14 11:08:46 -0600

    If / else statements

---
+++ ruby_challenges/if_else.rb
@@ -0,0 +1,27 @@
+if 1 + 1 == 2 
+    puts "1 and 1 does equal 2"
+end
+
+my_name = 'Gary'
+if my_name == 'Gary'
+    puts "Yo, Gary!"
+else 
+    puts "Oops, I thought your name was Gary. Sorry about that #{my_name}!"
+end
+
+fav_color = 'orange'
+if (fav_color == 'blue')
+    puts "Blue like the sky"
+elsif (fav_color == 'orange')
+    puts "Orange like a pumpkin"
+elsif (fav_color == 'yellow')
+    puts "Yellow like a daffodil"
+elsif (fav_color == 'green')
+    puts "Grean like grass"
+elsif (fav_color == 'black')
+    puts "Black like the night"
+elsif ( fav_color == 'purple')
+    puts "Purple like purple things...."
+else 
+    puts "No clue what your favorite color is..."
+end
